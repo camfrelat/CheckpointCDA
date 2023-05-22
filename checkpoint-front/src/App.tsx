64f6a9b34937decb95fdd172176b6e975c2618e4
camille <camfrelat@gmail.com>
2023-05-22 11:12:08 +0200

    navigation basics

--- checkpoint-front/src/App.tsx
+++ checkpoint-front/src/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Continents from "./pages/Continents/Continents";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Countries from "./pages/Countries";
 
 function App() {
   return (
     <>
-      <div>NAVBAR</div>
-      <Continents />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/continents" element={<Continents />} />
+          <Route path="/continents" element={<Countries />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ checkpoint-front/src/components/NavBar.tsx
@@ -0,0 +1,72 @@
+import { Menu, MenuProps } from "antd";
+import { AppstoreOutlined, MailOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
+const items: MenuProps["items"] = [
+  {
+    label: <Link to="/continents">Continents</Link>,
+    key: "mail",
+    icon: <MailOutlined />,
+  },
+  {
+    label: <Link to="/Countries">Countries</Link>,
+    key: "app",
+    icon: <AppstoreOutlined />,
+  },
+  //   {
+  //     label: "test",
+  //     key: "SubMenu",
+  //     icon: <SettingOutlined />,
+  //     children: [
+  //       {
+  //         type: "group",
+  //         label: "Item 1",
+  //         children: [
+  //           {
+  //             label: "Option 1",
+  //             key: "setting:1",
+  //           },
+  //           {
+  //             label: "Option 2",
+  //             key: "setting:2",
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         type: "group",
+  //         label: "Item 2",
+  //         children: [
+  //           {
+  //             label: "Option 3",
+  //             key: "setting:3",
+  //           },
+  //           {
+  //             label: "Option 4",
+  //             key: "setting:4",
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: (
+  //       <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+  //         Navigation Four - Link
+  //       </a>
+  //     ),
+  //     key: "alipay",
+  //   },
+];
+
+function NavBar() {
+  return (
+    <Menu
+      //   onClick={onClick}
+      //   selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+}
+
+export default NavBar;
